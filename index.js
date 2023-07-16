0dcf90b16f29d24e9fefb74992ea2d5124961659
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-07-16 01:06:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT ?? 3001;
+
+if (!process.env.PROXIED_URL) {
+    console.log("Missing 'PROXIED_URL' environment variable!");
+    process.exit(1);
+}
+
+const proxiedUrl = new URL(process.env.PROXIED_URL)
+
+function createNotification(alert, messageString) {
+    const message = JSON.parse(messageString);
+    const messageKey = message.options.message;
+    const titleKey = message.options.title;
+    return {
+        label: message.label,
+        options: {
+            from: "Grafana",
+            message: alert.annotations[messageKey] ?? "<no msg>",
+            title: alert.annotations[titleKey]
+        }
+    };
+}
+
+async function sendNotification(notification, auth) {
+    let res;
+    try {
+        res = await fetch(proxiedUrl, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "authorization": auth
+            },
+            body: JSON.stringify(notification)
+        });
+        return res.status;
+    } catch(err) {
+        console.error(new Date().toLocaleDateString(), err);
+        return 502;
+    }
+}
+
+async function sendNotifications(notifications, auth) {
+    let n = [];
+
+    for(const notification of notifications) {
+        n.push(sendNotification(notification, auth));
+    }
+
+    return await Promise.all(n);
+}
+
+app.use(express.json())
+app.post("/", async (req, res) => {
+    const notifications = [];
+
+    for(const alert of req.body.alerts) {
+        if (alert.status === "firing") {
+            notifications.push(createNotification(alert, req.body.message));
+        }
+        console.log(alert);
+    }
+
+    let statuses = await sendNotifications(notifications, req.headers.authorization);
+
+    for(const status of statuses) {
+        if (status !== 200) {
+            console.error(statuses);
+            return res.status(502).send();
+        }
+    }
+
+    res.status(200).send();
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening | Proxying [POST http://localhost:${PORT}] to [POST ${proxiedUrl.href}]`);
+})
